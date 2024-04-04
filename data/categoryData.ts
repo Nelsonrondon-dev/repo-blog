d5f68a67f7f418c7657a8e59cd0948c379719d2e
Nelson Rondon <nelsonrondon36@gmail.com>
2024-04-03 23:11:30 -0400

    first commit

---
+++ data/categoryData.ts
@@ -0,0 +1,28 @@
+const categoryData = [
+  {
+    id: 1,
+    title: 'Canada',
+    imgSrc: '/static/images/nuevaYork.jpg',
+    href: '/tags/canada',
+  },
+  {
+    id: 2,
+    title: 'Nueva York',
+    imgSrc: '/static/images/nuevaYork.jpg',
+    href: 'https://www.google.com',
+  },
+  {
+    id: 3,
+    title: 'Nueva York',
+    imgSrc: '/static/images/nuevaYork.jpg',
+    href: 'https://www.google.com',
+  },
+  {
+    id: 4,
+    title: 'Nueva York',
+    imgSrc: '/static/images/nuevaYork.jpg',
+    href: 'https://www.google.com',
+  },
+]
+
+export default categoryData
